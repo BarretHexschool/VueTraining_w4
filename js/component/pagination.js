1c3a3f28fd4a8fc39c7f0eb479468934d3438d9d
Barret <49317634+Barret0720@users.noreply.github.com>
2023-03-02 00:03:48 +0800

    Add files via upload

--- js/component/pagination.js
+++ js/component/pagination.js
@@ -3,7 +3,7 @@ export default {
   template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item" :class="{disabled: !pageData.has_pre}" >
-        <a class="page-link" href="#" @click.prevent="getProducts(page-1)" aria-label="Previous">
+        <a class="page-link" href="#" @click.prevent="getProducts(pageData.current_page-1)" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -11,7 +11,7 @@ export default {
       <li class="page-item" :class = "{active: page == pageData.current_page}" v-for="page in pageData.total_pages" :key="page + 'page'"><a class="page-link" href="#" @click.prevent="getProducts(page)" >{{page}}</a></li>
 
       <li class="page-item" :class="{disabled: !pageData.has_next}" >
-        <a class="page-link" href="#" @click.prevent="getProducts(page+1)"  aria-label="Next">
+        <a class="page-link" href="#" @click.prevent="getProducts(pageData.current_page+1)"  aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
